b3999f3861b9993234b4349b038a8452bc357505
Shah Sayem Ahmad <68594531+ShahSayem@users.noreply.github.com>
2025-05-23 01:15:35 +0600

    Update cipher_tool.py

--- cipher_tool.py
+++ cipher_tool.py
@@ -180,8 +180,7 @@ if cipher_choice == "Vernam Cipher":
         plaintext = st.text_area("Enter Plaintext")
         if st.button("🔒 Encrypt") and plaintext:
             cleaned_text, removed_chars = clean_unsupported(plaintext)
-            if removed_chars:
-                st.warning(f"Removed unsupported characters!!!")
+
             if not cleaned_text:
                 st.error("Plaintext is empty after cleaning.")
             else:
